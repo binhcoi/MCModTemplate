320b9b07a72647bc9f9888775c157ecf190b7733
binhcoi <12677277+binhcoi@users.noreply.github.com>
2020-12-22 20:18:58 -0800

    initial commit

---
+++ src/main/java/com/binhcoi/mcmods/modid/TemplateMod.java
@@ -0,0 +1,10 @@
+package com.binhcoi.mcmods.modid;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TemplateMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Hello world!");
+    }
+}
